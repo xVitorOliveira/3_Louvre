c845f809d19d90ead5aacaead6718d211ef749f3
Enielka <a.kleine@student.isartdigital.com>
2018-11-12 01:00:32 +0100

    Nav réparé

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,12 +1,10 @@
     <header>
-        <nav>
-            <ul>                
-                <li id="home"><a href="index.php" title="3Louvre" >3Louvre</a></li>
-                <li><a href="quartier.php"        title="Quartier">Quartier</a></li> <!-- vitor -->
-                <li><a href="culture_loisirs.php" title="Culture & Loisirs">Culture</a></li> <!-- Alexandre tout autor du quartier-->
-                <li><a href="infos_pratiques.php" title="Infos Pratiques">Infos</a></li> <!-- Alexis Autour et localization -->
-                <li><a href="evenements.php"      title="Evenements">Evenements</a></li> <!-- Lecatelier -->
-                <li><a href="louvre.php"          title="Louvre">Louvre</a></li> <!-- Jonathan -->
-            </ul>
+        <nav>                
+            <a id="home" href="index.php" title="3Louvre" >3Louvre</a>
+            <a href="quartier.php"        title="Quartier">Quartier</a> <!-- vitor -->
+            <a href="culture_loisirs.php" title="Culture & Loisirs">Culture</a> <!-- Alexandre tout autor du quartier-->
+            <a href="infos_pratiques.php" title="Infos Pratiques">Infos</a> <!-- Alexis Autour et localization -->
+            <a href="evenements.php"      title="Evenements">Evenements</a> <!-- Lecatelier -->
+            <a href="louvre.php"          title="Louvre">Louvre</a> <!-- Jonathan -->
         </nav>
     </header>
