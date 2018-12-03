9d165531c3a7526df4dbaa5f27cca99737c47bda
voliveira <vitoroliveiraalmeida97@outlook.com>
2018-12-03 17:52:39 +0100

    V1

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -17,12 +17,4 @@
                 <li><a href="evenements.php"      title="Evenements"><?php echo $evenements; ?></a></li> <!-- Lecatelier -->
                 <li><a href="louvre.php"          title="Louvre">Louvre</a></li> <!-- Jonathan -->
             </ul>
-        <nav>                
-            <a id="home" href="index.php" title="3Louvre" >3Louvre</a>
-            <a href="quartier.php"        title="Quartier">Quartier</a> <!-- vitor -->
-            <a href="culture_loisirs.php" title="Culture & Loisirs">Culture</a> <!-- Alexandre tout autor du quartier-->
-            <a href="infos_pratiques.php" title="Infos Pratiques">Infos</a> <!-- Alexis Autour et localization -->
-            <a href="evenements.php"      title="Evenements">Evenements</a> <!-- Lecatelier -->
-            <a href="louvre.php"          title="Louvre">Louvre</a> <!-- Jonathan -->
-          </nav>
     </header>
